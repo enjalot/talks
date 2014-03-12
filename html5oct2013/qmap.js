472e44e3120a443ce62aaf28868d5c3f29ed80a5
Ian Johnson <enjalot@gmail.com>
2014-03-11 17:52:43 -0700

    adding dataalchemy talk

---
+++ html5oct2013/qmap.js
@@ -0,0 +1,12 @@
+var qmap = {
+  time: "Timestamp",
+  pb: "What type of peanut butter do you prefer?",
+  toothpaste: "Where do you squeeze the toothpaste from?",
+  cv: "Vanilla or Chocolate?",
+  tp: "What direction does toilet paper go on the holder thingy?",
+  evil: "Where do you sit on the good vs. evil scale?",
+  oj: "Do you prefer pulp or no pulp in your orange juice?",
+  age: "How old are you?",
+  passion: "How passionate of a person are you?",
+  exp: "How many years have you been building for the web?"
+}
